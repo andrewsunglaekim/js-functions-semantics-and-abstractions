c21a38430b0b67d3b908712aa4da64b60d003441
Andrew <akim@redventures.com>
2020-04-02 21:47:42 -0400

    updates first-exercise

--- utils/buildMessages.js
+++
@@ -1,9 +0,0 @@
-function buildMessages(data){
-  const messagesEl = document.querySelector('.messages')
-  // messagesEl.appendChild(el)
-  const { messages } = data;
-  messages.forEach((message) => {
-    const messageView = new MessageView(message)
-    messagesEl.appendChild(messageView.el);
-  });
-}
--- utils/parseTime.js
+++
@@ -1,6 +0,0 @@
-function parseTime(dateTimeString) {
-  const date = luxon.DateTime.fromISO(dateTimeString);
-  const amPM = date.hour > 12 ? 'pm' :'am';
-  const hour = date.hour > 12 ? date.hour % 12 : date.hour;
-  return `${date.day} ${date.monthShort} ${hour}:${date.minute} ${amPM}`
-}
